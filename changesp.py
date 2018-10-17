130159b054948f292003a1f5d22d6debb7e8e408
anshubhaiyya <a.lahoti@gmail.com>
2018-10-17 13:42:41 +0100

    initial upload

---
+++ changesp.py
@@ -0,0 +1,41 @@
+import json
+import requests, base64
+import os                             # Allows for the clearing of the Terminal Window
+import time, datetime
+import glob
+import getpass
+# -*- coding: utf-8 -*-
+
+###  Getting Inputs from the user
+TOKENHERE = ""
+spainListusers =   []
+
+###  Define the API Arguments, and choose if you need a folder or not
+
+import sys
+if (3,0) <= sys.version_info < (4,0):
+    import http.client as httplib
+elif (2,6) <= sys.version_info < (3,0):
+    import httplib
+
+headers = {
+    "Authorization": "Bearer TOKENHERE",
+    "Content-Type": "application/json"}
+
+for i in range(len(spainListusers)):
+    params = {
+        "shared_folder_id": "123456",
+        "member": {".tag":"email","email":spainListusers[i]},
+        "access_level": {".tag":"viewer"}
+    }
+    c = httplib.HTTPSConnection("api.dropboxapi.com")
+    c.request("POST", "/2/sharing/update_folder_member", json.dumps(params), headers)
+    r = c.getresponse()
+    print(str(r.status) + " " + r.read() + "for email: " + spainListusers[i])
+###  API responses, first if an error occurs for reporting and second to show the new doc URL
+
+
+###  API responses, first if an error occurs for reporting and second to show the new doc URL
+
+    
+    
